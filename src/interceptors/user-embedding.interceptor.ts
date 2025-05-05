58869683efd23be5401b6b3847308119ed67e4f2
duong1090 <duong1090@gmail.com>
2025-05-05 17:28:43 +0700

    fix: add log

--- src/interceptors/user-embedding.interceptor.ts
+++ src/interceptors/user-embedding.interceptor.ts
@@ -1,14 +1,16 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common'
 import { Observable, tap } from 'rxjs'
 import { UserService } from '@modules/index-service'
 
 @Injectable()
 export class UserEmbeddingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(UserEmbeddingInterceptor.name)
   constructor(private readonly userService: UserService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       tap(async data => {
+        this.logger.log(`Preparing data to enqueue for embeddings: ${data}`)
         if (data && data._id) {
           await this.userService.enqueueUserForEmbedding(data._id.toString())
         }
